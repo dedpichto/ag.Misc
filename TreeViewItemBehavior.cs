20c3364ad2d352cde9b44d6a8a1d645847b6942a
Ded Pichto <39455845+dedpichto@users.noreply.github.com>
2026-01-03 15:18:09 +0200

    Refactor TreeViewItemBehavior to TreeViewBehavior

--- TreeViewItemBehavior.cs
+++ TreeViewItemBehavior.cs
@@ -1,70 +1,3 @@
-public static class TreeViewItemBehavior
-{
-    public static readonly DependencyProperty SelectParentOnClickProperty =
-        DependencyProperty.RegisterAttached(
-            "SelectParentOnClick",
-            typeof(bool),
-            typeof(TreeViewItemBehavior),
-            new PropertyMetadata(false, OnSelectParentOnClickChanged));
-
-    public static bool GetSelectParentOnClick(DependencyObject obj) => (bool)obj.GetValue(SelectParentOnClickProperty);
-    public static void SetSelectParentOnClick(DependencyObject obj, bool value) => obj.SetValue(SelectParentOnClickProperty, value);
-
-    private static void OnSelectParentOnClickChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        if (d is TreeViewItem item)
-        {
-            if ((bool)e.NewValue)
-            {
-                item.PreviewMouseLeftButtonDown += Item_PreviewMouseLeftButtonDown;
-                item.PreviewKeyDown += Item_PreviewKeyDown;
-            }
-            else
-            {
-                item.PreviewMouseLeftButtonDown -= Item_PreviewMouseLeftButtonDown;
-                item.PreviewKeyDown -= Item_PreviewKeyDown;
-            }
-        }
-    }
-
-    private static void Item_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-        SelectParentAndHandle(sender, () => e.Handled = true);
-    }
-
-    private static void Item_PreviewKeyDown(object sender, KeyEventArgs e)
-    {
-        if (e.Key == Key.Enter || e.Key == Key.Space)
-        {
-            SelectParentAndHandle(sender, () => e.Handled = true);
-        }
-    }
-
-    private static void SelectParentAndHandle(object sender, Action markHandled)
-    {
-        if (sender is not TreeViewItem childItem)
-            return;
-
-        // Find parent TreeViewItem
-        var parent = VisualTreeHelper.GetParent(childItem);
-        while (parent != null && parent is not TreeViewItem)
-        {
-            parent = VisualTreeHelper.GetParent(parent);
-        }
-
-        if (parent is TreeViewItem parentItem)
-        {
-            parentItem.IsSelected = true;
-            parentItem.BringIntoView();
-            markHandled();
-        }
-    }
-
-    ////////////////////////////////////////////////////
-
-    
-}
-
 public static class TreeViewBehavior
 {
     public static readonly DependencyProperty SelectedItemProperty =
@@ -72,25 +5,51 @@ public static class TreeViewBehavior
             "SelectedItem",
             typeof(object),
             typeof(TreeViewBehavior),
-            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnSelectedItemChanged));
+            new FrameworkPropertyMetadata(
+                null,
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                OnSelectedItemChanged,
+                CoerceSelectedItem));  // <-- add this
 
     public static object GetSelectedItem(DependencyObject obj) => obj.GetValue(SelectedItemProperty);
     public static void SetSelectedItem(DependencyObject obj, object value) => obj.SetValue(SelectedItemProperty, value);
+
+    // Coerce always runs, even null -> null
+    private static object CoerceSelectedItem(DependencyObject d, object baseValue)
+    {
+        if (d is TreeView treeView)
+        {
+            EnsureSubscribed(treeView);
+        }
+        return baseValue;
+    }
+
+    private static void EnsureSubscribed(TreeView treeView)
+    {
+        // Check if already subscribed using a private attached property
+        if (treeView.GetValue(IsSubscribedProperty) is bool subscribed && subscribed)
+            return;
+
+        treeView.SetValue(IsSubscribedProperty, true);
+
+        treeView.Loaded += TreeView_Loaded;
+        treeView.IsVisibleChanged += TreeView_IsVisibleChanged;
+        treeView.SelectedItemChanged += TreeView_SelectedItemChanged;
+    }
+
+    // Private marker to avoid double subscription
+    private static readonly DependencyProperty IsSubscribedProperty =
+        DependencyProperty.RegisterAttached(
+            "IsSubscribed",
+            typeof(bool),
+            typeof(TreeViewBehavior),
+            new PropertyMetadata(false));
 
     private static void OnSelectedItemChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         if (d is not TreeView treeView)
             return;
 
-        // First time: subscribe to events
-        treeView.Loaded -= TreeView_Loaded;
-        treeView.Loaded += TreeView_Loaded;
-        treeView.IsVisibleChanged -= TreeView_IsVisibleChanged;
-        treeView.IsVisibleChanged += TreeView_IsVisibleChanged;
-        treeView.SelectedItemChanged -= TreeView_SelectedItemChanged;
-        treeView.SelectedItemChanged += TreeView_SelectedItemChanged;
-
-        // Only sync if already loaded and visible
         if (treeView.IsLoaded && treeView.IsVisible)
         {
             SyncSelection(treeView, e.NewValue);
@@ -116,7 +75,9 @@ public static class TreeViewBehavior
     private static void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
     {
         if (sender is TreeView treeView)
+        {
             SetSelectedItem(treeView, e.NewValue);
+        }
     }
 
     private static void SyncSelection(TreeView treeView, object item)
