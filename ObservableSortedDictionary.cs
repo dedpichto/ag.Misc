d55fd7a207b0614bafada1f0508b5c578cc119d8
Ded Pichto <39455845+dedpichto@users.noreply.github.com>
2026-08-30 14:19:27 +0300

    Update ObservableSortedDictionary.cs

--- ObservableSortedDictionary.cs
+++ ObservableSortedDictionary.cs
@@ -1,34 +1,27 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 
-namespace WpfApp.Collections
+namespace ShayCommon.Helpers
 {
     /// <summary>
     /// Represents a collection of key-value pairs sorted by key,
     /// that raises change notifications (suitable for DataGrid binding in WPF).
     /// </summary>
-    public class ObservableSortedDictionary<TKey, TValue> : 
-        IDictionary<TKey, TValue>, 
-        INotifyCollectionChanged, 
+    public class ObservableSortedDictionary<TKey, TValue> :
+        IDictionary<TKey, TValue>,
+        INotifyCollectionChanged,
         INotifyPropertyChanged
     {
         private readonly SortedList<TKey, TValue> _dictionary;
 
-        public ObservableSortedDictionary()
-        {
-            _dictionary = new SortedList<TKey, TValue>();
-        }
-
-        public ObservableSortedDictionary(IComparer<TKey> comparer)
-        {
-            _dictionary = new SortedList<TKey, TValue>(comparer);
-        }
+        #region ctor
+        public ObservableSortedDictionary() => _dictionary = [];
+        public ObservableSortedDictionary(IComparer<TKey> comparer) => _dictionary = new SortedList<TKey, TValue>(comparer); 
+        #endregion
 
         #region IDictionary Implementation
-
         public TValue this[TKey key]
         {
             get => _dictionary[key];
@@ -37,16 +30,15 @@ namespace WpfApp.Collections
                 if (_dictionary.TryGetValue(key, out var oldValue))
                 {
                     if (EqualityComparer<TValue>.Default.Equals(oldValue, value)) return;
-                    
+
                     _dictionary[key] = value;
-                    int index = _dictionary.IndexOfKey(key);
-                    
-                    OnPropertyChanged(nameof(Values));
+                    var index = _dictionary.IndexOfKey(key);
+
                     OnPropertyChanged("Item[]");
                     OnCollectionChanged(new NotifyCollectionChangedEventArgs(
-                        NotifyCollectionChangedAction.Replace, 
-                        new KeyValuePair<TKey, TValue>(key, value), 
-                        new KeyValuePair<TKey, TValue>(key, oldValue), 
+                        NotifyCollectionChangedAction.Replace,
+                        new KeyValuePair<TKey, TValue>(key, value),
+                        new KeyValuePair<TKey, TValue>(key, oldValue),
                         index));
                 }
                 else
@@ -55,64 +47,48 @@ namespace WpfApp.Collections
                 }
             }
         }
-
         public ICollection<TKey> Keys => _dictionary.Keys;
         public ICollection<TValue> Values => _dictionary.Values;
         public int Count => _dictionary.Count;
         public bool IsReadOnly => false;
-
         public void Add(TKey key, TValue value)
         {
             _dictionary.Add(key, value);
-            int index = _dictionary.IndexOfKey(key);
-
+            var index = _dictionary.IndexOfKey(key);
             OnPropertyChanged(nameof(Count));
-            OnPropertyChanged(nameof(Keys));
-            OnPropertyChanged(nameof(Values));
             OnPropertyChanged("Item[]");
             OnCollectionChanged(new NotifyCollectionChangedEventArgs(
-                NotifyCollectionChangedAction.Add, 
-                new KeyValuePair<TKey, TValue>(key, value), 
+                NotifyCollectionChangedAction.Add,
+                new KeyValuePair<TKey, TValue>(key, value),
                 index));
         }
-
         public bool Remove(TKey key)
         {
-            if (_dictionary.TryGetValue(key, out var value))
-            {
-                int index = _dictionary.IndexOfKey(key);
-                if (_dictionary.Remove(key))
-                {
-                    OnPropertyChanged(nameof(Count));
-                    OnPropertyChanged(nameof(Keys));
-                    OnPropertyChanged(nameof(Values));
-                    OnPropertyChanged("Item[]");
-                    OnCollectionChanged(new NotifyCollectionChangedEventArgs(
-                        NotifyCollectionChangedAction.Remove, 
-                        new KeyValuePair<TKey, TValue>(key, value), 
-                        index));
-                    return true;
-                }
-            }
-            return false;
-        }
+            if (!_dictionary.TryGetValue(key, out var value)) return false;
 
+            var index = _dictionary.IndexOfKey(key);
+            _dictionary.Remove(key);
+            OnPropertyChanged(nameof(Count));
+            OnPropertyChanged("Item[]");
+            OnCollectionChanged(new NotifyCollectionChangedEventArgs(
+                NotifyCollectionChangedAction.Remove,
+                new KeyValuePair<TKey, TValue>(key, value),
+                index));
+            return true;
+        }
         public bool ContainsKey(TKey key) => _dictionary.ContainsKey(key);
         public bool TryGetValue(TKey key, out TValue value) => _dictionary.TryGetValue(key, out value);
         public void Clear()
         {
+            if (_dictionary.Count == 0) return;
             _dictionary.Clear();
             OnPropertyChanged(nameof(Count));
-            OnPropertyChanged(nameof(Keys));
-            OnPropertyChanged(nameof(Values));
             OnPropertyChanged("Item[]");
             OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
         }
-
         #endregion
 
         #region ICollection Implementation
-
         public void Add(KeyValuePair<TKey, TValue> item) => Add(item.Key, item.Value);
         public bool Contains(KeyValuePair<TKey, TValue> item) => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).Contains(item);
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).CopyTo(array, arrayIndex);
@@ -124,31 +100,18 @@ namespace WpfApp.Collections
             }
             return false;
         }
-
         #endregion
 
         #region IEnumerable Implementation
-
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => _dictionary.GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
         #endregion
 
         #region INotify events
-
         public event NotifyCollectionChangedEventHandler CollectionChanged;
         public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
-        {
-            CollectionChanged?.Invoke(this, e);
-        }
-
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
+        protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs e) => CollectionChanged?.Invoke(this, e);
+        protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         #endregion
     }
 }
